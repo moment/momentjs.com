5e41a7c239f94337f529a6e31c013d4464220893
Tim Wood <washwithcare@gmail.com>
2012-07-14 09:12:39 -0700

    removing grunt from local node_modules

--- grunt.js
+++ grunt.js
@@ -3,9 +3,16 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         concat: {
-            dist: {
-                src: ['src/intro.js', 'src/project.js', 'src/outro.js'],
-                dest: 'dist/built.js'
+            tests: {
+                src: [
+                    'source/js/iife_start.js',
+                    'libs/nodeunit/nodeunit.js',
+                    'libs/moment/test/moment/*.js',
+                    'libs/moment/test/lang/*.js',
+                    'source/js/test.js',
+                    'source/js/iife_end.js'
+                ],
+                dest: 'js/tests.js'
             }
         }
     });
