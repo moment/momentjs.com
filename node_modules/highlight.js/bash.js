a62cc34083985bffc6cf4b7555d1b001c39da061
Tim Wood <washwithcare@gmail.com>
2012-07-25 11:45:30 -0700

    adding highlight.js to node_modules

---
+++ node_modules/highlight.js/bash.js
@@ -0,0 +1,58 @@
+module.exports = function(hljs) {
+  var BASH_LITERAL = 'true false';
+  var VAR1 = {
+    className: 'variable',
+    begin: '\\$([a-zA-Z0-9_]+)\\b'
+  };
+  var VAR2 = {
+    className: 'variable',
+    begin: '\\$\\{(([^}])|(\\\\}))+\\}',
+    contains: [hljs.C_NUMBER_MODE]
+  };
+  var QUOTE_STRING = {
+    className: 'string',
+    begin: '"', end: '"',
+    illegal: '\\n',
+    contains: [hljs.BACKSLASH_ESCAPE, VAR1, VAR2],
+    relevance: 0
+  };
+  var APOS_STRING = {
+    className: 'string',
+    begin: '\'', end: '\'',
+    contains: [{begin: '\'\''}],
+    relevance: 0
+  };
+  var TEST_CONDITION = {
+    className: 'test_condition',
+    begin: '', end: '',
+    contains: [QUOTE_STRING, APOS_STRING, VAR1, VAR2, hljs.C_NUMBER_MODE],
+    keywords: {
+      literal: BASH_LITERAL
+    },
+    relevance: 0
+  };
+
+  return {
+    defaultMode: {
+      keywords: {
+        keyword: 'if then else fi for break continue while in do done echo exit return set declare',
+        literal: BASH_LITERAL
+      },
+      contains: [
+        {
+          className: 'shebang',
+          begin: '(#!\\/bin\\/bash)|(#!\\/bin\\/sh)',
+          relevance: 10
+        },
+        VAR1,
+        VAR2,
+        hljs.HASH_COMMENT_MODE,
+        hljs.C_NUMBER_MODE,
+        QUOTE_STRING,
+        APOS_STRING,
+        hljs.inherit(TEST_CONDITION, {begin: '\\[ ', end: ' \\]', relevance: 0}),
+        hljs.inherit(TEST_CONDITION, {begin: '\\[\\[ ', end: ' \\]\\]'})
+      ]
+    }
+  };
+};
