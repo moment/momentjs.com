a6c90634255a5e7ca9ece42b78ca6fecd86b4a87
Weldan Jamili <mweldan@gmail.com>
2013-02-24 01:38:43 +0800

    add bahasa melayu

--- js/tests.js
+++ js/tests.js
@@ -8663,6 +8663,267 @@ exports["lang:zh-tw"] = {
         test.done();
     }
 };
+var moment = require("../../moment");
+
+
+    /**************************************************
+      Bahasa Melayu
+     *************************************************/
+
+exports["lang:ms-my"] = {
+    "parse" : function(test) {
+        test.expect(96);
+        moment.lang('ms-my');
+        var i,
+            tests = 'Januari Jan_Februari Feb_Mac Mac_April Apr_Mei Mei_Jun Jun_Julai Jul_Ogos Ogs_September Sep_Oktober Okt_November Nov_Disember Dis'.split("_");
+
+        function equalTest(input, mmm, i) {
+            test.equal(moment(input, mmm).month(), i, input + ' sepatutnya bulan ' + (i + 1));
+        }
+
+        for (i = 0; i < 12; i++) {
+            tests[i] = tests[i].split(' ');
+            equalTest(tests[i][0], 'MMM', i);
+            equalTest(tests[i][1], 'MMM', i);
+            equalTest(tests[i][0], 'MMMM', i);
+            equalTest(tests[i][1], 'MMMM', i);
+            equalTest(tests[i][0].toLocaleLowerCase(), 'MMMM', i);
+            equalTest(tests[i][1].toLocaleLowerCase(), 'MMMM', i);
+            equalTest(tests[i][0].toLocaleUpperCase(), 'MMMM', i);
+            equalTest(tests[i][1].toLocaleUpperCase(), 'MMMM', i);
+        }
+
+        test.done();
+    },
+
+    "format" : function(test) {
+        test.expect(18);
+        moment.lang('ms-my');
+        var a = [
+                ['dddd, MMMM Do YYYY, h:mm:ss a',      'Ahad, Februari 14 2010, 3:25:50 petang'],
+                ['ddd, hA',                            'Ahd, 3petang'],
+                ['M Mo MM MMMM MMM',                   '2 2 02 Februari Feb'],
+                ['YYYY YY',                            '2010 10'],
+                ['D Do DD',                            '14 14 14'],
+                ['d do dddd ddd',                   '0 0 Ahad Ahd'],
+                ['DDD DDDo DDDD',                      '45 45 045'],
+                ['w wo ww',                            '8 8 08'],
+                ['h hh',                               '3 03'],
+                ['H HH',                               '15 15'],
+                ['m mm',                               '25 25'],
+                ['s ss',                               '50 50'],
+                ['a A',                                'petang petang'],
+                ['\\h\\\ari ke DDDo t\\a\\hun ini', 'hari ke 45 tahun ini'],
+                ['L',                                  '14/02/2010'],
+                ['LL',                                 '14 Februari 2010'],
+                ['LLL',                                '14 Februari 2010 pukul 15.25'],
+                ['LLLL',                               'Ahad, 14 Februari 2010 pukul 15.25']
+            ],
+
+        b = moment(new Date(2010, 1, 14, 15, 25, 50, 125)),
+        i;
+        for (i = 0; i < a.length; i++) {
+            test.equal(b.format(a[i][0]), a[i][1], a[i][0] + ' ---> ' + a[i][1]);
+        }
+        test.done();
+    },
+
+    "format ordinal" : function(test) {
+        test.expect(31);
+        moment.lang('ms-my');
+        test.equal(moment([2011, 0, 1]).format('DDDo'), '1', '1');
+        test.equal(moment([2011, 0, 2]).format('DDDo'), '2', '2');
+        test.equal(moment([2011, 0, 3]).format('DDDo'), '3', '3');
+        test.equal(moment([2011, 0, 4]).format('DDDo'), '4', '4');
+        test.equal(moment([2011, 0, 5]).format('DDDo'), '5', '5');
+        test.equal(moment([2011, 0, 6]).format('DDDo'), '6', '6');
+        test.equal(moment([2011, 0, 7]).format('DDDo'), '7', '7');
+        test.equal(moment([2011, 0, 8]).format('DDDo'), '8', '8');
+        test.equal(moment([2011, 0, 9]).format('DDDo'), '9', '9');
+        test.equal(moment([2011, 0, 10]).format('DDDo'), '10', '10');
+
+        test.equal(moment([2011, 0, 11]).format('DDDo'), '11', '11');
+        test.equal(moment([2011, 0, 12]).format('DDDo'), '12', '12');
+        test.equal(moment([2011, 0, 13]).format('DDDo'), '13', '13');
+        test.equal(moment([2011, 0, 14]).format('DDDo'), '14', '14');
+        test.equal(moment([2011, 0, 15]).format('DDDo'), '15', '15');
+        test.equal(moment([2011, 0, 16]).format('DDDo'), '16', '16');
+        test.equal(moment([2011, 0, 17]).format('DDDo'), '17', '17');
+        test.equal(moment([2011, 0, 18]).format('DDDo'), '18', '18');
+        test.equal(moment([2011, 0, 19]).format('DDDo'), '19', '19');
+        test.equal(moment([2011, 0, 20]).format('DDDo'), '20', '20');
+
+        test.equal(moment([2011, 0, 21]).format('DDDo'), '21', '21');
+        test.equal(moment([2011, 0, 22]).format('DDDo'), '22', '22');
+        test.equal(moment([2011, 0, 23]).format('DDDo'), '23', '23');
+        test.equal(moment([2011, 0, 24]).format('DDDo'), '24', '24');
+        test.equal(moment([2011, 0, 25]).format('DDDo'), '25', '25');
+        test.equal(moment([2011, 0, 26]).format('DDDo'), '26', '26');
+        test.equal(moment([2011, 0, 27]).format('DDDo'), '27', '27');
+        test.equal(moment([2011, 0, 28]).format('DDDo'), '28', '28');
+        test.equal(moment([2011, 0, 29]).format('DDDo'), '29', '29');
+        test.equal(moment([2011, 0, 30]).format('DDDo'), '30', '30');
+
+        test.equal(moment([2011, 0, 31]).format('DDDo'), '31', '31');
+        test.done();
+    },
+
+    "format month" : function(test) {
+        test.expect(12);
+        moment.lang('ms-my');
+        var i,
+            expected = 'Januari Jan_Februari Feb_Mac Mac_April Apr_Mei Mei_Jun Jun_Julai Jul_Ogos Ogs_September Sep_Oktober Okt_November Nov_Disember Dis'.split("_");
+
+        for (i = 0; i < expected.length; i++) {
+            test.equal(moment([2011, i, 1]).format('MMMM MMM'), expected[i], expected[i]);
+        }
+        test.done();
+    },
+
+    "format week" : function(test) {
+        test.expect(7);
+        moment.lang('ms-my');
+        var i,
+            expected = 'Ahad Ahd_Isnin Isn_Selasa Sel_Rabu Rab_Khamis Kha_Jumaat Jum_Sabtu Sab'.split("_");
+
+        for (i = 0; i < expected.length; i++) {
+            test.equal(moment([2011, 0, 2 + i]).format('dddd ddd'), expected[i], expected[i]);
+        }
+
+        test.done();
+    },
+
+    "from" : function(test) {
+        test.expect(30);
+        moment.lang('ms-my');
+        var start = moment([2007, 1, 28]);
+
+        test.equal(start.from(moment([2007, 1, 28]).add({s:44}), true),  "beberapa saat", "44 saat = beberapa saat");
+        test.equal(start.from(moment([2007, 1, 28]).add({s:45}), true),  "seminit",      "45 saat = seminit");
+        test.equal(start.from(moment([2007, 1, 28]).add({s:89}), true),  "seminit",      "89 saat = seminit");
+        test.equal(start.from(moment([2007, 1, 28]).add({s:90}), true),  "2 minit",     "90 saat = 2 minit");
+        test.equal(start.from(moment([2007, 1, 28]).add({m:44}), true),  "44 minit",    "44 minit = 44 minit");
+        test.equal(start.from(moment([2007, 1, 28]).add({m:45}), true),  "sejam",       "45 minit = sejam");
+        test.equal(start.from(moment([2007, 1, 28]).add({m:89}), true),  "sejam",       "89 minit = sejam");
+        test.equal(start.from(moment([2007, 1, 28]).add({m:90}), true),  "2 jam",       "90 minit = 2 jam");
+        test.equal(start.from(moment([2007, 1, 28]).add({h:5}), true),   "5 jam",       "5 jam = 5 jam");
+        test.equal(start.from(moment([2007, 1, 28]).add({h:21}), true),  "21 jam",      "21 jam = 21 jam");
+        test.equal(start.from(moment([2007, 1, 28]).add({h:22}), true),  "sehari",         "22 jam = sehari");
+        test.equal(start.from(moment([2007, 1, 28]).add({h:35}), true),  "sehari",         "35 jam = sehari");
+        test.equal(start.from(moment([2007, 1, 28]).add({h:36}), true),  "2 hari",        "36 jam = 2 hari");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:1}), true),   "sehari",         "1 hari = sehari");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:5}), true),   "5 hari",        "5 hari = 5 hari");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:25}), true),  "25 hari",       "25 hari = 25 hari");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:26}), true),  "sebulan",       "26 hari = sebulan");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:30}), true),  "sebulan",       "30 hari = sebulan");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:45}), true),  "sebulan",       "45 hari = sebulan");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:46}), true),  "2 bulan",      "46 hari = 2 bulan");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:74}), true),  "2 bulan",      "75 hari = 2 bulan");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:76}), true),  "3 bulan",      "76 hari = 3 bulan");
+        test.equal(start.from(moment([2007, 1, 28]).add({M:1}), true),   "sebulan",       "1 bulan = sebulan");
+        test.equal(start.from(moment([2007, 1, 28]).add({M:5}), true),   "5 bulan",      "5 bulan = 5 bulan");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:344}), true), "11 bulan",     "344 hari = 11 bulan");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:345}), true), "setahun",        "345 hari = setahun");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:547}), true), "setahun",        "547 hari = setahun");
+        test.equal(start.from(moment([2007, 1, 28]).add({d:548}), true), "2 tahun",       "548 hari = 2 tahun");
+        test.equal(start.from(moment([2007, 1, 28]).add({y:1}), true),   "setahun",        "1 tahun = setahun");
+        test.equal(start.from(moment([2007, 1, 28]).add({y:5}), true),   "5 tahun",       "5 tahun = 5 tahun");
+
+        test.done();
+    },
+
+    "suffix" : function(test) {
+        test.expect(2);
+        moment.lang('ms-my');
+        test.equal(moment(30000).from(0), "dalam beberapa saat",  "prefix");
+        test.equal(moment(0).from(30000), "beberapa saat yang lepas", "suffix");
+
+        test.done();
+    },
+
+    "now from now" : function(test) {
+        test.expect(1);
+        moment.lang('ms-my');
+        test.equal(moment().fromNow(), "beberapa saat yang lepas",  "waktu sekarang dari sekarang sepatutnya menunjukkan sebagai telah lepas");
+
+        test.done();
+    },
+
+    "fromNow" : function(test) {
+        test.expect(2);
+        moment.lang('ms-my');
+        test.equal(moment().add({s:30}).fromNow(), "dalam beberapa saat", "dalam beberapa saat");
+        test.equal(moment().add({d:5}).fromNow(), "dalam 5 hari", "dalam 5 hari");
+
+        test.done();
+    },
+
+    "calendar day" : function(test) {
+        test.expect(6);
+        moment.lang('ms-my');
+        var a = moment().hours(2).minutes(0).seconds(0);
+
+        test.equal(moment(a).calendar(),                     "Hari ini pukul 02.00",     "hari ini pada waktu yang sama");
+        test.equal(moment(a).add({ m: 25 }).calendar(),      "Hari ini pukul 02.25",     "Sekarang tambah 25 minit");
+        test.equal(moment(a).add({ h: 1 }).calendar(),       "Hari ini pukul 03.00",     "Sekarang tambah 1 jam");
+        test.equal(moment(a).add({ d: 1 }).calendar(),       "Esok pukul 02.00",  "esok pada waktu yang sama");
+        test.equal(moment(a).subtract({ h: 1 }).calendar(),  "Hari ini pukul 01.00",     "Sekarang tolak 1 jam");
+        test.equal(moment(a).subtract({ d: 1 }).calendar(),  "Kelmarin pukul 02.00", "kelmarin pada waktu yang sama");
+
+        test.done();
+    },
+
+    "calendar next week" : function(test) {
+        test.expect(15);
+        moment.lang('ms-my');
+        var i, m;
+
+        for (i = 2; i < 7; i++) {
+            m = moment().add({ d: i });
+            test.equal(m.calendar(),       m.format('dddd [pukul] LT'),  "Hari ini + " + i + " hari waktu sekarang");
+            m.hours(0).minutes(0).seconds(0).milliseconds(0);
+            test.equal(m.calendar(),       m.format('dddd [pukul] LT'),  "Hari ini + " + i + " hari permulaan hari");
+            m.hours(23).minutes(59).seconds(59).milliseconds(999);
+            test.equal(m.calendar(),       m.format('dddd [pukul] LT'),  "Hari ini + " + i + " hari tamat hari");
+        }
+        test.done();
+    },
+
+    "calendar last week" : function(test) {
+        test.expect(15);
+        moment.lang('ms-my');
+        var i, m;
+
+        for (i = 2; i < 7; i++) {
+            m = moment().subtract({ d: i });
+            test.equal(m.calendar(),       m.format('dddd [lepas] [pukul] LT'),  "Hari ini - " + i + " hari waktu sekarang");
+            m.hours(0).minutes(0).seconds(0).milliseconds(0);
+            test.equal(m.calendar(),       m.format('dddd [lepas] [pukul] LT'),  "Hari ini - " + i + " hari permulaan hari");
+            m.hours(23).minutes(59).seconds(59).milliseconds(999);
+            test.equal(m.calendar(),       m.format('dddd [lepas] [pukul] LT'),  "Hari ini - " + i + " hari tamat hari");
+        }
+        test.done();
+    },
+
+    "calendar all else" : function(test) {
+        test.expect(4);
+        moment.lang('ms-my');
+        var weeksAgo = moment().subtract({ w: 1 }),
+            weeksFromNow = moment().add({ w: 1 });
+
+        test.equal(weeksAgo.calendar(),       weeksAgo.format('L'),  "1 minggu lepas");
+        test.equal(weeksFromNow.calendar(),   weeksFromNow.format('L'),  "dalam 1 minggu");
+
+        weeksAgo = moment().subtract({ w: 2 });
+        weeksFromNow = moment().add({ w: 2 });
+
+        test.equal(weeksAgo.calendar(),       weeksAgo.format('L'),  "2 minggu lepas");
+        test.equal(weeksFromNow.calendar(),   weeksFromNow.format('L'),  "dalam 2 minggu");
+
+        test.done();
+    }
+};
+
 (function(){
     var banner = $('#nodeunit-banner');
     var tests = $('#nodeunit-tests');
