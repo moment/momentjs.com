3d97d46c0d9c678ccfed05233b443b79a29366ac
Iskren Chernev <iskren.chernev@gmail.com>
2013-08-04 23:39:24 -0700

    Added module name to bug report

--- source/js/test.js
+++ source/js/test.js
@@ -9,6 +9,7 @@
 	var passed = 0;
 	var failed = 0;
 	var total = 0;
+	var currentTestModule;
 
 	function updateTest(_passed, _failed) {
 		passed += _passed;
@@ -28,6 +29,7 @@
 	nodeunit.runModules(exports, {
 		moduleStart : function (name) {
 			tests.append('<h3>' + name + '</h3>');
+			currentTestModule = name;
 		},
 		testDone : function (name, assertions) {
 			var testEl = $('<li>'),
@@ -56,6 +58,7 @@
 			for (var i = 0; i < assertions.length; i++) {
 				assert = assertions[i];
 				assert.test_name = name;
+				assert.module_name = currentTestModule;
 				assertLi = $('<li>');
 				assertHtml = '<strong>' + total + '.' + (i + 1) + '</strong> ';
 				assertHtml += (assert.message || assert.method || 'no message');
@@ -77,11 +80,12 @@
 		done : function (assertions) {
 			var duration = moment().diff(start),
 				failures = assertions.failures(),
-				assert, error, i, preText, toStr, lastTestName;
+				assert, error, i,
+				toStr, header, lastHeader,
 
-				header = "Please <a href='https://github.com/moment/moment/issues/new'>submit an issue</a> " +
-						"to moment&quot;s github repo with the following content:";
-				titleText = "" + failures + " test" + (failures !== 1 ? "s" : "") + " failed";
+				headerText = "Please <a href='https://github.com/moment/moment/issues/new'>submit an issue</a> " +
+						"to moment&quot;s github repo with the following content:",
+				titleText = "" + failures + " test" + (failures !== 1 ? "s" : "") + " failed",
 				bodyText = [
 					"### Client info",
 					"Date.prototype.toString = " + (new Date()).toString(),
@@ -95,10 +99,11 @@
 					error = assert.error;
 					if (assert.failed()) {
 						bodyText.push('');
-						if (lastTestName !== assert.test_name) {
-							lastTestName = assert.test_name;
+						header = "" + assert.module_name + " - " + assert.test_name;
+						if (lastHeader !== header) {
+							lastHeader = header;
 							bodyText.push('====');
-							bodyText.push('### ' + lastTestName);
+							bodyText.push('### ' + lastHeader);
 						}
 						bodyText.push(assert.message);
 						bodyText.push('(' + error.expected + ' ' +
@@ -108,7 +113,7 @@
 						bodyText.push('```');
 					}
 				}
-				toStr = '<p>' + header + '</p>' +
+				toStr = '<p>' + headerText + '</p>' +
 						'<pre>' + titleText + '</pre>' +
 						'<pre>' + bodyText.join('</br>') + '</pre>';
 
