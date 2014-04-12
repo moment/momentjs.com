ab863689de5adc5ca8b191a05e7844ac2c7c894c
Iskren Chernev <iskren.chernev@gmail.com>
2014-04-12 10:23:26 -0700

    Build

--- static/js/global.js
+++ static/js/global.js
@@ -8481,14 +8481,14 @@
 }));
 
 // moment-timezone.js
-// version : 0.0.3
+// version : 0.0.5
 // author : Tim Wood
 // license : MIT
 // github.com/timrwood/moment-timezone
 
 (function () {
 
-	var VERSION = "0.0.3";
+	var VERSION = "0.0.5";
 
 	function onload(moment) {
 		var oldZoneName = moment.fn.zoneName,
