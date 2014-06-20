36273f9b0e0bd98f64fc5f096f69bcf3601862ea
Tim Wood <washwithcare@gmail.com>
2014-06-20 10:18:33 -0700

    Add download buttons for moment-timezone plus zone

--- data/size.js
+++ data/size.js
@@ -1,7 +1,7 @@
 var fs       = require('fs'),
 	gzipSize = require('gzip-size'),
-	moment   = require('../libs/moment/moment.js'),
-	timezone = require('../libs/moment-timezone/moment-timezone.js').tz;
+	moment   = require('../libs/moment'),
+	timezone = require('../libs/moment-timezone').tz;
 
 function size (file) {
 	var src = fs.readFileSync('./libs/' + file + '.js', 'utf8');
@@ -23,6 +23,7 @@ module.exports = {
 	moment_timezone_with_data_min           : size('moment-timezone/builds/moment-timezone-with-data.min'),
 	moment_timezone_with_data_2010_2020     : size('moment-timezone/builds/moment-timezone-with-data-2010-2020'),
 	moment_timezone_with_data_2010_2020_min : size('moment-timezone/builds/moment-timezone-with-data-2010-2020.min'),
-	moment_version          : moment.version,
-	moment_timezone_version : timezone.version
+	moment_version                          : moment.version,
+	moment_timezone_version                 : timezone.version,
+	moment_timezone_data_version            : timezone.dataVersion
 };
