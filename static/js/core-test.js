eccbb0446fb840f740a58d88cfc02a4a06eb61b9
Iskren Chernev <iskren.chernev@gmail.com>
2016-02-03 14:57:09 -0800

    Updates

--- static/js/core-test.js
+++ static/js/core-test.js
@@ -4688,7 +4688,7 @@ QUnit.config.autostart = false;
     }
 
     // ASP.NET json date format regex
-    var aspNetRegex = /(\-)?(?:(\d*)[. ])?(\d+)\:(\d+)(?:\:(\d+)\.?(\d{3})?)?/;
+    var aspNetRegex = /^(\-)?(?:(\d*)[. ])?(\d+)\:(\d+)(?:\:(\d+)\.?(\d{3})?\d*)?$/;
 
     // from http://docs.closure-library.googlecode.com/git/closure_goog_date_date.js.source.html
     // somewhat more in line with 4.4.3.2 2004 spec, but allows decimal anywhere
@@ -6445,12 +6445,12 @@ QUnit.config.autostart = false;
     ;
 
     //! moment.js
-    //! version : 2.11.1
+    //! version : 2.11.2
     //! authors : Tim Wood, Iskren Chernev, Moment.js contributors
     //! license : MIT
     //! momentjs.com
 
-    utils_hooks__hooks.version = '2.11.1';
+    utils_hooks__hooks.version = '2.11.2';
 
     setHookCallback(local__createLocal);
 
