0fa81ff3fec3c57cd93dcf6bd9c86f6dab2ba7d6
Iskren Chernev <iskren.chernev@gmail.com>
2016-06-26 01:33:44 -0700

    Autogen locales

--- data/locale.js
+++ data/locale.js
@@ -1,109 +1,411 @@
-var locales = {
-    'af'     : 'Afrikaans',
-    'ar'     : 'Arabic',
-    'ar-ma'  : 'Arabic (Morocco)',
-    'ar-sa'  : 'Arabic (Saudi Arabia)',
-    'ar-tn'  : 'Arabic (Tunisia)',
-    'az'     : 'Azerbaijani',
-    'be'     : 'Belarusian',
-    'bg'     : 'Bulgarian',
-    'bn'     : 'Bengali',
-    'bo'     : 'Tibetan',
-    'br'     : 'Breton',
-    'bs'     : 'Bosnian',
-    'ca'     : 'Catalan',
-    'cs'     : 'Czech',
-    'cv'     : 'Chuvash',
-    'cy'     : 'Welsh',
-    'da'     : 'Danish',
-    'de'     : 'German',
-    'de-at'  : 'German (Austria)',
-    'dv'     : 'Maldivian',
-    'el'     : 'Greek',
-    'en'     : 'English (United States)',
-    'en-au'  : 'English (Australia)',
-    'en-ca'  : 'English (Canada)',
-    'en-gb'  : 'English (United Kingdom)',
-    'en-ie'  : 'English (Ireland)',
-    'en-nz'  : 'English (New Zealand)',
-    'eo'     : 'Esperanto',
-    'es'     : 'Spanish',
-    'et'     : 'Estonian',
-    'eu'     : 'Basque',
-    'fa'     : 'Persian',
-    'fi'     : 'Finnish',
-    'fo'     : 'Faroese',
-    'fr'     : 'French',
-    'fr-ca'  : 'French (Canada)',
-    'fr-ch'  : 'French (Switzerland)',
-    'fy'     : 'Frisian',
-    'gd'     : 'Scottish Gaelic',
-    'gl'     : 'Galician',
-    'he'     : 'Hebrew',
-    'hi'     : 'Hindi',
-    'hr'     : 'Croatian',
-    'hu'     : 'Hungarian',
-    'hy-am'  : 'Armenian',
-    'id'     : 'Bahasa Indonesia',
-    'is'     : 'Icelandic',
-    'it'     : 'Italian',
-    'ja'     : 'Japanese',
-    'jv'     : 'Javanese',
-    'ka'     : 'Georgian',
-    'kk'     : 'Kazakh',
-    'km'     : 'Khmer (Cambodia)',
-    'ko'     : 'Korean',
-    'lb'     : 'Luxembourgish',
-    'lo'     : 'Lao',
-    'lt'     : 'Lithuanian',
-    'lv'     : 'Latvian',
-    'me'     : 'Montenegrin',
-    'mk'     : 'Macedonian',
-    'ml'     : 'Malayalam',
-    'mr'     : 'Marathi',
-    'ms-my'  : 'Bahasa Melayu (Malaysia)',
-    'my'     : 'Burmese',
-    'nb'     : 'Norwegian',
-    'ne'     : 'Nepalese',
-    'nl'     : 'Dutch',
-    'nn'     : 'Norwegian Nynorsk',
-    'pl'     : 'Polish',
-    'pt'     : 'Portuguese',
-    'pt-br'  : 'Portuguese (Brazil)',
-    'ro'     : 'Romanian',
-    'ru'     : 'Russian',
-    'se'     : 'Northern Sami',
-    'si'     : 'Sinhalese',
-    'sk'     : 'Slovak',
-    'sl'     : 'Slovenian',
-    'sq'     : 'Albanian',
-    'sr'     : 'Serbian',
-    'sr-cyrl': 'Serbian Cyrillic',
-    'sv'     : 'Swedish',
-    'sw'     : 'Swahili',
-    'ta'     : 'Tamil',
-    'te'     : 'Telugu',
-    'th'     : 'Thai',
-    'tl-ph'  : 'Tagalog (Filipino)',
-    'tlh'    : 'Klingon',
-    'tr'     : 'Turkish',
-    'tzl'    : 'Talossan',
-    'tzm'    : 'Tamaziɣt',
-    'tzm-latn': 'Tamaziɣt Latin',
-    'uk'     : 'Ukrainian',
-    'uz'     : 'Uzbek',
-    'vi'     : 'Vietnamese',
-    'zh-cn'  : 'Chinese (Simplified)',
-    'zh-tw'  : 'Chinese (Traditional)'
-};
-
-module.exports = Object.keys(locales).map(function(key){
-    return {
-        abbr : key,
-        name : locales[key]
-    };
-}).sort(function (a, b) {
-    if (a.name < b.name) { return -1; }
-    if (a.name > b.name) { return 1; }
-    return 0;
-});
+// this file is auto generated from tasks/update-locales.js every release
+module.export =[
+    {
+        "abbr": "af",
+        "name": "Afrikaans"
+    },
+    {
+        "abbr": "sq",
+        "name": "Albanian"
+    },
+    {
+        "abbr": "ar",
+        "name": "Arabic"
+    },
+    {
+        "abbr": "ar-ma",
+        "name": "Arabic (Morocco)"
+    },
+    {
+        "abbr": "ar-sa",
+        "name": "Arabic (Saudi Arabia)"
+    },
+    {
+        "abbr": "ar-tn",
+        "name": "Arabic (Tunisia)"
+    },
+    {
+        "abbr": "hy-am",
+        "name": "Armenian"
+    },
+    {
+        "abbr": "az",
+        "name": "Azerbaijani"
+    },
+    {
+        "abbr": "eu",
+        "name": "Basque"
+    },
+    {
+        "abbr": "be",
+        "name": "Belarusian"
+    },
+    {
+        "abbr": "bn",
+        "name": "Bengali"
+    },
+    {
+        "abbr": "bs",
+        "name": "Bosnian"
+    },
+    {
+        "abbr": "br",
+        "name": "Breton"
+    },
+    {
+        "abbr": "bg",
+        "name": "Bulgarian"
+    },
+    {
+        "abbr": "my",
+        "name": "Burmese"
+    },
+    {
+        "abbr": "km",
+        "name": "Cambodian"
+    },
+    {
+        "abbr": "ca",
+        "name": "Catalan"
+    },
+    {
+        "abbr": "tzm",
+        "name": "Central Atlas Tamazight"
+    },
+    {
+        "abbr": "tzm-latn",
+        "name": "Central Atlas Tamazight Latin"
+    },
+    {
+        "abbr": "zh-cn",
+        "name": "Chinese (China)"
+    },
+    {
+        "abbr": "zh-tw",
+        "name": "Chinese (Taiwan)"
+    },
+    {
+        "abbr": "cv",
+        "name": "Chuvash"
+    },
+    {
+        "abbr": "hr",
+        "name": "Croatian"
+    },
+    {
+        "abbr": "cs",
+        "name": "Czech"
+    },
+    {
+        "abbr": "da",
+        "name": "Danish"
+    },
+    {
+        "abbr": "nl",
+        "name": "Dutch"
+    },
+    {
+        "abbr": "en",
+        "name": "English"
+    },
+    {
+        "abbr": "en-au",
+        "name": "English (Australia)"
+    },
+    {
+        "abbr": "en-ca",
+        "name": "English (Canada)"
+    },
+    {
+        "abbr": "en-ie",
+        "name": "English (Ireland)"
+    },
+    {
+        "abbr": "en-nz",
+        "name": "English (New Zealand)"
+    },
+    {
+        "abbr": "en-gb",
+        "name": "English (United Kingdom)"
+    },
+    {
+        "abbr": "eo",
+        "name": "Esperanto"
+    },
+    {
+        "abbr": "et",
+        "name": "Estonian"
+    },
+    {
+        "abbr": "fo",
+        "name": "Faroese"
+    },
+    {
+        "abbr": "fi",
+        "name": "Finnish"
+    },
+    {
+        "abbr": "fr",
+        "name": "French"
+    },
+    {
+        "abbr": "fr-ca",
+        "name": "French (Canada)"
+    },
+    {
+        "abbr": "fr-ch",
+        "name": "French (Switzerland)"
+    },
+    {
+        "abbr": "fy",
+        "name": "Frisian"
+    },
+    {
+        "abbr": "gl",
+        "name": "Galician"
+    },
+    {
+        "abbr": "ka",
+        "name": "Georgian"
+    },
+    {
+        "abbr": "de",
+        "name": "German"
+    },
+    {
+        "abbr": "de-at",
+        "name": "German (Austria)"
+    },
+    {
+        "abbr": "el",
+        "name": "Greek"
+    },
+    {
+        "abbr": "he",
+        "name": "Hebrew"
+    },
+    {
+        "abbr": "hi",
+        "name": "Hindi"
+    },
+    {
+        "abbr": "hu",
+        "name": "Hungarian"
+    },
+    {
+        "abbr": "is",
+        "name": "Icelandic"
+    },
+    {
+        "abbr": "id",
+        "name": "Indonesian"
+    },
+    {
+        "abbr": "it",
+        "name": "Italian"
+    },
+    {
+        "abbr": "jv",
+        "name": "Japanese"
+    },
+    {
+        "abbr": "ja",
+        "name": "Japanese"
+    },
+    {
+        "abbr": "kk",
+        "name": "Kazakh"
+    },
+    {
+        "abbr": "tlh",
+        "name": "Klingon"
+    },
+    {
+        "abbr": "ko",
+        "name": "Korean"
+    },
+    {
+        "abbr": "ky",
+        "name": "Kyrgyz"
+    },
+    {
+        "abbr": "lo",
+        "name": "Lao"
+    },
+    {
+        "abbr": "lv",
+        "name": "Latvian"
+    },
+    {
+        "abbr": "lt",
+        "name": "Lithuanian"
+    },
+    {
+        "abbr": "lb",
+        "name": "Luxembourgish"
+    },
+    {
+        "abbr": "mk",
+        "name": "Macedonian"
+    },
+    {
+        "abbr": "ms-my",
+        "name": "Malay"
+    },
+    {
+        "abbr": "ms",
+        "name": "Malay"
+    },
+    {
+        "abbr": "ml",
+        "name": "Malayalam"
+    },
+    {
+        "abbr": "dv",
+        "name": "Maldivian"
+    },
+    {
+        "abbr": "mr",
+        "name": "Marathi"
+    },
+    {
+        "abbr": "me",
+        "name": "Montenegrin"
+    },
+    {
+        "abbr": "ne",
+        "name": "Nepalese"
+    },
+    {
+        "abbr": "se",
+        "name": "Northern Sami"
+    },
+    {
+        "abbr": "nb",
+        "name": "Norwegian Bokmål"
+    },
+    {
+        "abbr": "nn",
+        "name": "Nynorsk"
+    },
+    {
+        "abbr": "fa",
+        "name": "Persian"
+    },
+    {
+        "abbr": "pl",
+        "name": "Polish"
+    },
+    {
+        "abbr": "pt",
+        "name": "Portuguese"
+    },
+    {
+        "abbr": "pt-br",
+        "name": "Portuguese (Brazil)"
+    },
+    {
+        "abbr": "x-pseudo",
+        "name": "Pseudo"
+    },
+    {
+        "abbr": "pa-in",
+        "name": "Punjabi (India)"
+    },
+    {
+        "abbr": "ro",
+        "name": "Romanian"
+    },
+    {
+        "abbr": "ru",
+        "name": "Russian"
+    },
+    {
+        "abbr": "gd",
+        "name": "Scottish Gaelic"
+    },
+    {
+        "abbr": "sr",
+        "name": "Serbian"
+    },
+    {
+        "abbr": "sr-cyrl",
+        "name": "Serbian Cyrillic"
+    },
+    {
+        "abbr": "si",
+        "name": "Sinhalese"
+    },
+    {
+        "abbr": "sk",
+        "name": "Slovak"
+    },
+    {
+        "abbr": "sl",
+        "name": "Slovenian"
+    },
+    {
+        "abbr": "es",
+        "name": "Spanish"
+    },
+    {
+        "abbr": "es-do",
+        "name": "Spanish (Dominican Republic)"
+    },
+    {
+        "abbr": "sw",
+        "name": "Swahili"
+    },
+    {
+        "abbr": "ss",
+        "name": "Swazi"
+    },
+    {
+        "abbr": "sv",
+        "name": "Swedish"
+    },
+    {
+        "abbr": "tl-ph",
+        "name": "Tagalog (Philippines)"
+    },
+    {
+        "abbr": "tzl",
+        "name": "Talossan"
+    },
+    {
+        "abbr": "ta",
+        "name": "Tamil"
+    },
+    {
+        "abbr": "te",
+        "name": "Telugu"
+    },
+    {
+        "abbr": "th",
+        "name": "Thai"
+    },
+    {
+        "abbr": "bo",
+        "name": "Tibetan"
+    },
+    {
+        "abbr": "tr",
+        "name": "Turkish"
+    },
+    {
+        "abbr": "uk",
+        "name": "Ukrainian"
+    },
+    {
+        "abbr": "uz",
+        "name": "Uzbek"
+    },
+    {
+        "abbr": "vi",
+        "name": "Vietnamese"
+    },
+    {
+        "abbr": "cy",
+        "name": "Welsh"
+    }
+];
---
+++ tasks/update-locales.js
@@ -0,0 +1,48 @@
+module.exports = function (grunt) {
+	var fs = require('fs'),
+		join = require('path').join;
+
+	grunt.registerTask(
+			'update-locales',
+			'update data/locales.js to match locales defined in moment/src/locales',
+			function () {
+				var localesDir = 'libs/moment/src/locale',
+					locales = {en: 'English (United States)'},
+					numErrors = 0;
+				fs.readdirSync(localesDir).forEach(function (localeFile) {
+					var localeName = null, localeAbbr = null;
+					grunt.file.read(join(localesDir, localeFile)).split(/\n|\r|\r\n/).forEach(function (line) {
+						if (line.slice(0, 10) === '//! locale') {
+							localeName = line.split(' : ')[1].split(' [')[0].trim();
+							localeAbbr = line.split(' : ')[1].split(/\[|\]/)[1];
+						}
+					});
+					if (localeName == null) {
+						grunt.log.error('wtf', localeFile);
+						numErrors += 1;
+					} else if (localeAbbr !== localeFile.split('.')[0]) {
+						grunt.log.error('wtf2', localeFile, localeAbbr, localeName);
+						numErrors += 1;
+					} else {
+						// grunt.log.writeln(localeAbbr, localeName);
+						locales[localeAbbr] = localeName;
+					}
+				});
+				if (numErrors > 0) {
+					return;
+				}
+				grunt.file.write('data/locale.js',
+						'// this file is auto generated from tasks/update-locales.js every release\n' +
+						'module.export =' + JSON.stringify(Object.keys(locales).map(function (key) {
+					return {
+						abbr: key,
+						name: locales[key]
+					}
+				}).sort(function (a, b) {
+					if (a.name < b.name) { return -1; }
+					if (a.name > b.name) { return 1; }
+					return 0;
+				}), null, 4) + ';\n');
+			}
+	);
+};
