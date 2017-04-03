43e864d4d91efb29d253bde02c248d074ea16269
Matt Johnson <mj1856@hotmail.com>
2017-04-02 20:31:09 -0700

    Fix build errors due to year change on moment-timezone

--- data/size.js
+++ data/size.js
@@ -21,8 +21,8 @@ module.exports = {
 	moment_timezone_min                     : size('moment-timezone/builds/moment-timezone.min'),
 	moment_timezone_with_data               : size('moment-timezone/builds/moment-timezone-with-data'),
 	moment_timezone_with_data_min           : size('moment-timezone/builds/moment-timezone-with-data.min'),
-	moment_timezone_with_data_2010_2020     : size('moment-timezone/builds/moment-timezone-with-data-2010-2020'),
-	moment_timezone_with_data_2010_2020_min : size('moment-timezone/builds/moment-timezone-with-data-2010-2020.min'),
+	moment_timezone_with_data_2012_2022     : size('moment-timezone/builds/moment-timezone-with-data-2012-2022'),
+	moment_timezone_with_data_2012_2022_min : size('moment-timezone/builds/moment-timezone-with-data-2012-2022.min'),
 	moment_version                          : moment.version,
 	moment_timezone_version                 : timezone.version,
 	moment_timezone_data_version            : timezone.dataVersion
