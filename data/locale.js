df917a0d0631ea3057b74a9f21e8f090d96e845f
Kunal Marwaha <marwahaha@berkeley.edu>
2018-12-12 22:52:48 -0800

    Update moment to 2.23.0

--- data/locale.js
+++ data/locale.js
@@ -269,6 +269,10 @@ module.exports =[
         "name": "Korean"
     },
     {
+        "abbr": "ku",
+        "name": "Kurdish"
+    },
+    {
         "abbr": "ky",
         "name": "Kyrgyz"
     },
