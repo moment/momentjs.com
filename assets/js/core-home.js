a6195122bc320406fd3ae08b9fcfdb826a6264a8
Martin Vendl <vmarty89@gmail.com>
2022-08-22 12:21:46 +0200

    Fix clock hand movement

--- assets/js/core-home.js
+++ assets/js/core-home.js
@@ -26,8 +26,8 @@
     function updateClock(){
         var now = moment(),
             second = now.seconds() * 6,
-            minute = now.minutes() * 6 + second / 60,
-            hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
+            minute = now.minutes() * 6 + second / 10,
+            hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 2;
 
         $('#hour').css("transform", "rotate(" + hour + "deg)");
         $('#minute').css("transform", "rotate(" + minute + "deg)");
